31afdd5da9db9ec07c89df78a5c73c27681e408e
Michael Glavitsch <michael.glavitsch@gmail.com>
2020-03-08 15:44:46 +0100

    Import of dependency jumbler-go

--- dojumble/main.go
+++ dojumble/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	jumble "github.com/mglavitsch/jumble-go"
+	jumble "github.com/mglavitsch/jumbler-go"
 )
 
 const defaultText = "It doesn't matter in what order the letters in a word are, the only important thing is that the first and last letter be at the right place. The rest can be a total mess and you can still read it without problem. This is because the human mind does not read every letter by itself, but the word as a whole."
